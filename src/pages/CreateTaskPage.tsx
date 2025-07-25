8bb0f3be8d0303e30187e9b66e8f7232dba7d326
M. Sertaç Toröz <msertactoroz@gmail.com>
2025-07-25 16:35:46 +0300

    initialized task creation page and its types

---
+++ src/pages/CreateTaskPage.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+// import TaskForm from "../components/Task/TaskForm";
+const CreateTaskPage = () => {
+    return (
+        <div className="max-w-xl mx-auto mt-8 p-4">
+            <h1 className="text-2xl font-bold mb-4">Create New Task</h1>
+            {/* <TaskForm /> */}
+        </div>
+    );
+};
+
+export default CreateTaskPage;
